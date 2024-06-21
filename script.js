cb9b9a101540aa31b84725af8bd37b68abecb3b2
ijelectron <nemo316@naver.com>
2024-06-21 11:05:23 +0900

    2024062110

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const TRANSLATE_ENDPOINT = `https://translation.googleapis.com/language/translat
 const DETECT_LANGUAGE_ENDPOINT = `https://translation.googleapis.com/language/translate/v2/detect?key=${API_KEY}`;
 
 // 유튜브 검색 API 엔드포인트
-const YOUTUBE_SEARCH_ENDPOINT = `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&part=snippet&type=video&maxResults=8`; // maxResults=24으로 변경
+const YOUTUBE_SEARCH_ENDPOINT = `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&part=snippet&type=video&maxResults=8`;
 
 // 유튜브 채널 통계 API 엔드포인트
 const YOUTUBE_CHANNEL_ENDPOINT = `https://www.googleapis.com/youtube/v3/channels?key=${API_KEY}&part=statistics`;
@@ -23,22 +23,40 @@ let currentSortOption = 'views';
 
 // 24개의 이미지 파일 경로 리스트
 const imageFiles = [
-    'image/ad_1.png', 'image/ad_2.png', 'image/ad_3.png', 'image/ad_4.png', 
-    'image/ad_5.png', 'image/ad_6.png', 'image/ad_7.png', 'image/ad_8.png'
-  //  'image/ad_9.png', 'image/ad_10.png', 'image/ad_11.png', 'image/ad_12.png',
-  //  'image/ad_13.png', 'image/ad_14.png', 'image/ad_15.png', 'image/ad_16.png',
-  //  'image/ad_17.png', 'image/ad_18.png', 'image/ad_19.png', 'image/ad_20.png',
-  //  'image/ad_21.png', 'image/ad_22.png', 'image/ad_23.png', 'image/ad_24.png'
+    'ad/ad_1.png',
+    'ad/ad_2.png',
+    'ad/ad_3.png',
+    'ad/ad_4.png', 
+    'ad/ad_5.png',
+    'ad/ad_6.png',
+    'ad/ad_7.png',
+    'ad/ad_8.png',
+    'ad/ad_9.png',
+    'ad/ad_10.png',
+    'ad/ad_11.png',
+    'ad/ad_12.png',
+    'ad/ad_13.png',
+    'ad/ad_14.png',
+    'ad/ad_15.png',
+    'ad/ad_16.png',
+    'ad/ad_17.png',
+    'ad/ad_18.png',
+    'ad/ad_19.png',
+    'ad/ad_20.png',
+    'ad/ad_21.png',
+    'ad/ad_22.png',
+    'ad/ad_23.png',
+    'ad/ad_24.png'
 ];
 
 // 24개의 이미지 파일 링크 리스트
 const imageLinks = [
-    'https://ijelectron.modoo.at/?link=33k5h4x6', 'https://smartstore.naver.com/ijelectron316'//, 'https://link3.com', 'https://link4.com'
-  //  'https://link5.com', 'https://link6.com', 'https://link7.com', 'https://link8.com',
-  //  'https://link9.com', 'https://link10.com', 'https://link11.com', 'https://link12.com',
-  //  'https://link13.com', 'https://link14.com', 'https://link15.com', 'https://link16.com',
-  //  'https://link17.com', 'https://link18.com', 'https://link19.com', 'https://link20.com',
-  //  'https://link21.com', 'https://link22.com', 'https://link23.com', 'https://link24.com'
+    'https://ijelectron.modoo.at/?link=33k5h4x6',
+    'https://smartstore.naver.com/ijelectron316'
+    // 'https://link3.com',
+    // 'https://link4.com',
+    // ...
+    // 'https://link24.com'
 ];
 
 // 번역 및 유튜브 검색을 처리하는 함수
@@ -46,7 +64,7 @@ async function translateAndSearch(targetLang) {
     const query = document.getElementById('search-input').value;  // 검색창에서 입력된 값을 가져옴
     const detectedLang = await detectLanguage(query);  // 입력된 텍스트의 언어를 감지
     const translatedText = await translateText(query, targetLang);  // 입력된 텍스트를 목표 언어로 번역
-    searchYouTube(translatedText, targetLang);  // 번역된 텍스트로 유튜브 검색 수행, targetLang을 전달
+    searchYouTube(translatedText, targetLang);  // 번역된 텍스트로 유튜브 검색 수행
 }
 
 // 입력된 텍스트의 언어를 감지하는 함수
@@ -91,7 +109,7 @@ async function translateVideoDetails(details, targetLang) {
     return data.data.translations[0].translatedText;  // 번역된 텍스트 반환
 }
 
-// 구독자 수와 조회수를 포맷하는 함수
+// 숫자를 포맷하는 함수
 function formatNumber(number) {
     return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
@@ -132,6 +150,7 @@ async function downloadVideo(url) {
     }
 }
 
+// 오디오 다운로드 요청을 보내는 함수
 async function downloadAudio(url) {
     const requestData = {
         url: url,
@@ -163,7 +182,7 @@ async function displayVideos(videos, targetLang) {
     const container = document.getElementById('video-container');
     container.innerHTML = '';  // 기존 비디오 목록 초기화
 
-    // 비디오 목록을 최대 24개로 제한
+    // 비디오 목록을 제한
     const limitedVideos = videos.slice(0, 8);
 
     // 비디오의 조회수와 구독자 수 가져오기
@@ -222,11 +241,11 @@ async function displayVideos(videos, targetLang) {
 
         const button1 = document.createElement('button');
         button1.classList.add('video-button');
-        button1.addEventListener('click', () => downloadVideo(`https://www.youtube.com/watch?v=${videoId}`)); // button1 클릭 이벤트 추가
+        button1.addEventListener('click', () => downloadVideo(`https://www.youtube.com/watch?v=${videoId}`));  // button1 클릭 이벤트 추가
 
         const button2 = document.createElement('button');
         button2.classList.add('audio-button');
-        button2.addEventListener('click', () => downloadAudio(`https://www.youtube.com/watch?v=${videoId}`)); // button2 클릭 이벤트 추가
+        button2.addEventListener('click', () => downloadAudio(`https://www.youtube.com/watch?v=${videoId}`));  // button2 클릭 이벤트 추가
 
         buttonContainer.appendChild(button1);
         buttonContainer.appendChild(button2);
@@ -273,26 +292,24 @@ async function displayVideos(videos, targetLang) {
         slideButtonContainer.appendChild(slideButtonText);
         slideButtonContainer.appendChild(slideButton);
 
-        videoEntry.appendChild(slideButtonContainer); // 슬라이드 버튼 컨테이너를 비디오 항목에 추가
+        videoEntry.appendChild(slideButtonContainer);  // 슬라이드 버튼 컨테이너를 비디오 항목에 추가
 
-         // 이미지 요소 생성 및 추가
-         const imageLink = document.createElement('a');  // 링크 요소 생성
-         imageLink.href = imageLinks[i];  // 링크 경로 설정
-         imageLink.target = '_blank';  // 새 창에서 열리도록 설정
- 
-         const imageElement = document.createElement('img');
-         imageElement.src = imageFiles[i];  // 정렬된 순서에 따라 이미지 파일 경로 설정
-         imageElement.alt = 'Banner Ads : ijelectron316@naver.com';
-         imageElement.classList.add('video-image');
- 
-         imageLink.appendChild(imageElement);  // 이미지 요소를 링크에 추가
-         videoEntry.appendChild(imageLink);  // 링크 요소를 비디오 항목에 추가
- 
-         container.appendChild(videoEntry);  // 비디오 항목을 컨테이너에 추가
- 
-         buttonContainer.style.display = 'flex';
-     }
- }
+        // 이미지 요소 생성 및 추가
+        const imageLink = document.createElement('a');  // 링크 요소 생성
+        imageLink.href = imageLinks[i];  // 링크 경로 설정
+        imageLink.target = '_blank';  // 새 창에서 열리도록 설정
+
+        const imageElement = document.createElement('img');
+        imageElement.src = imageFiles[i];  // 정렬된 순서에 따라 이미지 파일 경로 설정
+        imageElement.alt = 'Banner Ads : ijelectron316@naver.com';
+        imageElement.classList.add('video-image');
+
+        imageLink.appendChild(imageElement);  // 이미지 요소를 링크에 추가
+        videoEntry.appendChild(imageLink);  // 링크 요소를 비디오 항목에 추가
+        container.appendChild(videoEntry);  // 비디오 항목을 컨테이너에 추가
+        buttonContainer.style.display = 'flex';
+    }
+}
 
 // 라디오 버튼의 선택에 따라 정렬 기준을 변경하는 이벤트 리스너 추가
 document.querySelectorAll('input[name="options"]').forEach(radio => {
@@ -302,7 +319,6 @@ document.querySelectorAll('input[name="options"]').forEach(radio => {
         translateAndSearch(query);  // 변경된 정렬 기준으로 다시 검색 수행
     });
 });
-
 
 // 번역 버튼을 클릭할 때 검색어가 없는 경우 새로고침
 function handleTranslateButtonClick(targetLang) {
@@ -316,4 +332,3 @@ function handleTranslateButtonClick(targetLang) {
 
 // 초기 검색 수행
 translateAndSearch(document.getElementById('search-input').value);
-
